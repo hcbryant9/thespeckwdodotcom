d9267984bb85390b693469612c45e253980e49b1
hcbryant9 <bryanthc@vcu.edu>
2023-05-19 15:42:04 -0400

    adding orange three d and flahed

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const switcher = document.querySelector('.btn');
 const musicBTN = document.querySelector('.music-btn');
 const videosBTN = document.querySelector('.videos-btn');
 const sheetsBTN = document.querySelector('.sheets-btn');
+const flashedBTN = document.querySelector('.flashed-btn');
+const threeBTN = document.querySelector('.three-btn');
+const orangeBTN = document.querySelector('.orange-btn');
 //adding event handler for the click event
 
 switcher.addEventListener('click', function(){
@@ -37,4 +40,16 @@ videosBTN.addEventListener('click',function(){
 sheetsBTN.addEventListener('click',function(){
     console.log('button clicked');
     window.location.href = "sheets.html";
+});
+orangeBTN.addEventListener('click',function(){
+    console.log('button clicked');
+    window.location.href = "orange.html";
+});
+threeBTN.addEventListener('click',function(){
+    console.log('button clicked');
+    window.location.href = "three.html";
+});
+flashedBTN.addEventListener('click',function(){
+    console.log('button clicked');
+    window.location.href = "flashed.html";
 });
