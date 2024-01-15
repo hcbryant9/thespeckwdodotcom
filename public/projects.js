30d82ec3186f480f1d11ab90d1c4213eab4407bb
hcbryant9 <bryanthc@vcu.edu>
2024-01-15 12:00:27 -0500

    updates to projects

--- public/projects.js
+++ public/projects.js
@@ -9,6 +9,7 @@ const huesBTN = document.getElementById("hues");
 const emotionalBTN = document.getElementById("emotional");
 const websiteBTN = document.getElementById("website");
 const aboutBTN = document.getElementById("about");
+const pumpBTN = document.getElementById("pump");
 //adding event handler for the click event
 
 
@@ -36,5 +37,9 @@ websiteBTN.addEventListener('click',function(){
 aboutBTN.addEventListener('click',function(){
     console.log('button clicked');
     window.location.href = "about.html";
+});
+pumpBTN.addEventListener('click',function(){
+    console.log('button clicked');
+    window.location.href = "pump.html";
 });
 
