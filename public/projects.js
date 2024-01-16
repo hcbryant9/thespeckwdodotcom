cfecf263475947240938748d79e08205fd3d15b3
hcbryant9 <bryanthc@vcu.edu>
2024-01-16 14:00:02 -0500

    adding project descriptions

--- public/projects.js
+++ public/projects.js
@@ -1,6 +1,9 @@
 'use strict';
 
 
+const projectOverview = document.getElementById("projectOverview");
+
+
 
 
 const kinectBTN = document.getElementById("kinect");
@@ -43,3 +46,37 @@ pumpBTN.addEventListener('click',function(){
     window.location.href = "pump.html";
 });
 
+
+
+// Adding event handler for the mouseover event
+kinectBTN.addEventListener('mouseover', function () {
+    updateOverview("kinect-pure-data-unity is a body synthesizer created to auralize the space we occupy", "Kinect v2, Pure Data, Unity, C#", "Summer 2023 - Fall 2023");
+});
+
+harmonyBTN.addEventListener('mouseover', function () {
+    updateOverview("virtual-soul-harmony is a mixed media exhibit presented 12/08/23. In the virtual world, users are in a forest while, in the physical world, they are standing on a roll of grass.", "Unity, Quest 2, Sod", "Fall 2023");
+});
+
+huesBTN.addEventListener('mouseover', function () {
+    updateOverview("guess-hue is a web game inspired by the board game Hues and Cues as well as NYT games such as Wordle and the Mini Cross", "React.js, HTML, CSS, Python", "December 2023 - Current");
+});
+
+emotionalBTN.addEventListener('mouseover', function () {
+    updateOverview("emotional-survaillence-ml is a machine learning project to raise awareness about the harm of ML and AI on the individual.", "Python, scikit-learn, pandas, Unity, Meta Quest Pro", "Fall 2023 - Current");
+});
+
+websiteBTN.addEventListener('mouseover', function () {
+    updateOverview("The website serves as an alternative platform to addictive media platforms. Hank1wdo.com is largely inspired by individual-blogs and various open-sourced communities. ", "JavaScript, HTML, CSS, Firebase, GitHub Actions", "Summer 2023 - Current");
+});
+
+pumpBTN.addEventListener('mouseover', function () {
+    updateOverview("In collaboration with 'Friends of Pumphouse', a VR tour of the long-term restoration project of the Richmond Pumphouse.", "Unity, C#, Maya, Meta Quest 2", "Fall 2023 - Current");
+});
+
+// Function to update the overview based on the project button
+// Function to update the overview based on the project button
+function updateOverview(overview, tools, timeline) {
+    projectOverview.innerHTML = "Overview: " + overview + "<br>";
+    projectOverview.innerHTML += "Tools: " + tools + "<br>";
+    projectOverview.innerHTML += "Timeline: " + timeline + "<br>"
+}
