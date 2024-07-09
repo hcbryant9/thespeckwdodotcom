832f2a3b3cfafa45050e3db2211aa56b8ed5ff7d
hcbryant9 <bryanthc@vcu.edu>
2024-07-09 11:34:31 -0400

    new index

--- public/app.js
+++ public/app.js
@@ -1,40 +1,13 @@
 'use strict';
 
-const dogImage = document.getElementById('hairyball');
-let rotationDegreeX = 0;
-let rotationDegreeY = 0;
-const switcher = document.querySelector('.btn');
-const videosBTN = document.getElementById("mp4s");
-const seoulBTN = document.getElementById("seoul");
-const flashedBTN = document.getElementById("flashed");
-const julyBTN = document.getElementById("july");
-const orangeBTN = document.getElementById("orange");
-const edinburghBTN = document.getElementById("edinburgh");
-const dylonBTN = document.getElementById("dylon");
-const jpgBTN = document.getElementById("jpgs");
-const projectsBTN = document.getElementById("projects");
-const technologyBTN = document.getElementById("technology");
+
+const videosBTN = document.getElementById("video-photo");
+
+const jpgBTN = document.getElementById("jpg-photo");
+const projectsBTN = document.getElementById("project-photo");
+
 //adding event handler for the click event
 
-switcher.addEventListener('click', function(){
-
-    document.body.classList.toggle('light-theme');
-    document.body.classList.toggle('dark-theme');
-
-    const className = document.body.className;
-    if(className == "light-theme"){
-        this.textContent = "dark";
-        
-    } else{
-        this.textContent = "light";
-    }
-    console.log('current class name: ' + className);
-    
-});
-technologyBTN.addEventListener('click',function(){
-    console.log('button clicked');
-    window.location.href = "technology.html";
-});
 
 videosBTN.addEventListener('click',function(){
     console.log('button clicked');
@@ -50,11 +23,3 @@ jpgBTN.addEventListener('click',function(){
     window.location.href = "jpgs.html";
 });
 
-function rotate3D() {
-    rotationDegreeX += 1; // Adjust the rotation angles as needed
-    rotationDegreeY += 2;
-    
-    dogImage.style.transform = `rotateX(${rotationDegreeX}deg) rotateY(${rotationDegreeY}deg)`;
-    requestAnimationFrame(rotate3D); // Continuously call the function
-}
-//rotate3D(); // Start the rotation animation
